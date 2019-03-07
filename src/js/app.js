33ce521c0bbda78398f70de8384b306a7e08e5b8
Daniel Voicu <daniel.voicu@brandfusion.ro>
2019-03-07 17:45:18 +0200

    fix
    bootstrap modal

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux';
 import Layout from "./components/Layout";
 import store from "./store";
 
+import "bootstrap/js/src/modal.js"
+
 
 const app = document.getElementById("app");
 ReactDOM.render(<Provider store={store}><Layout /></Provider>, app);
